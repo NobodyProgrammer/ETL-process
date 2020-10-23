03759c4422fe763061e019008677036796c81ab2
coomoneyText <zz20102ss@gmail.com>
2020-10-23 17:53:04 +0800

    finish the final version

--- multiThread.c
+++ multiThread.c
@@ -5,6 +5,8 @@
 #include<string.h>
 int column_count=0;
 int numStringInThread=0;
+FILE *input;
+FILE *output;
 struct threadContainer
 {
     int array[21];
@@ -39,6 +41,29 @@ void *child(void *arg){
     }
     pthread_exit(NULL);
 }
+void restString(int count,int rest){
+    if(count>0){
+        char *ptr=NULL;
+        for(int i=rest;i<count;++i){
+        
+            char *string=json[i].buffer;
+            //printf("%s",json[i].buffer); 
+            char *token=strtok_r(string,"|",&ptr);
+            
+            int k=0;
+            
+            while (token!=NULL)
+            {
+                json[i].array[k]=atoi(token);
+                //printf("%s|",token);
+                token=strtok_r(NULL,"|",&ptr);
+                ++k;
+            }
+            //printf("\n");
+        }
+
+    }
+}
 void spliteByMultiThread(threadContainer *json,int array_num,int thread_num){
     int *start_idx=malloc(sizeof(int)*thread_num);
     int now_idx=0;
@@ -63,18 +88,54 @@ void spliteByMultiThread(threadContainer *json,int array_num,int thread_num){
         //printf("thread %d finish\n",i);
     }
     //printf("\n");
+
     free(myThread);
     
+}
+void writeToJson(int count){
+    for(int i=0;i<count;++i){
+        // for(int k=0;k<20;++k){
+        
+        //     char *tmp=malloc(sizeof(char)*20);
+        //     if(k<19){
+        //         sprintf(tmp,"%d|",json[i].array[k]);
+        //         fputs(tmp,output);
+        //     }
+        //     else{
+        //         sprintf(tmp,"%d\n",json[i].array[k]);
+        //         fputs(tmp,output);
+        //     }
+        //     free(tmp);
+        // }
+
+
+
+
+        fwrite("    {\n",1,sizeof(char)*6,output);
+        for(int j=0;j<20;++j){
+            
+            char *tmp=malloc(sizeof(char)*50);
+            if(j<19){
+                sprintf(tmp,"       ""col_%d"":%d,\n",j+1,json[i].array[j]);
+                fwrite(tmp,1,strlen(tmp),output);
+            }
+            else{
+                sprintf(tmp,"       ""col_%d"":%d\n",j+1,json[i].array[j]);
+                fwrite(tmp,1,strlen(tmp),output);
+            }
+            free(tmp);
+        }
+        fwrite("    },\n",1,sizeof(char)*7,output);
+    }
 }
 int main(int argc,char *argv[]){
     printf("pid=%d\n",getpid());
     double spliteTime=0;
     clock_t start_t,end_t,total_t;
     char *output_file="output.json";
-    char *input_file="input2.csv";
+    char *input_file="input.csv";
     //int res=writeTojson(output_file);
-    FILE *input;
-    FILE *output;
+
     int array_num=20000;
     int count=0;
     int thread_num=atoi(argv[1]);//number of thread 
@@ -97,45 +158,19 @@ int main(int argc,char *argv[]){
             //here to use thread to strtok string
             //printf("successful\n");
             spliteByMultiThread(json,array_num,thread_num);
-            for(int i=0;i<array_num;++i){
-                // for(int k=0;k<20;++k){
-                
-                //     char *tmp=malloc(sizeof(char)*20);
-                //     if(k<19){
-                //         sprintf(tmp,"%d|",json[i].array[k]);
-                //         fputs(tmp,output);
-                //     }
-                //     else{
-                //         sprintf(tmp,"%d\n",json[i].array[k]);
-                //         fputs(tmp,output);
-                //     }
-                //     free(tmp);
-                // }
-                fwrite("    {\n",1,sizeof(char)*6,output);
-                for(int j=0;j<20;++j){
-                    
-                    char *tmp=malloc(sizeof(char)*50);
-                    if(j<19){
-                        sprintf(tmp,"       ""col_%d"":%d,\n",j+1,json[i].array[j]);
-                        fwrite(tmp,1,strlen(tmp),output);
-                    }
-                    else{
-                        sprintf(tmp,"       ""col_%d"":%d\n",j+1,json[i].array[j]);
-                        fwrite(tmp,1,strlen(tmp),output);
-                    }
-                    free(tmp);
-                }
-                fwrite("    },\n",1,sizeof(char)*7,output);
-            }
-
-
-
+            //如果thread不整除20000就有剩下的要ouput
+            int rest=numStringInThread*thread_num;
+            restString(array_num,rest);
+            writeToJson(array_num);
             count=0;
 
 
         }
         
     }
+    //printf("%d\n",count);
+    restString(count,0);
+    writeToJson(count);
     fwrite("]",1,sizeof(char),output);
     
 }
